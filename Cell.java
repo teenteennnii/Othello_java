93b805fc9267c1e9d599538db30086104dd1ee44
teenteennnii <teennopwijit@gmail.com>
2023-05-10 13:35:46 +0700

    add color

--- Cell.java
+++ Cell.java
@@ -1,15 +1,28 @@
 public class Cell {
     private boolean token;
+    private char discColor;
 
     public Cell() {
         token = false;
     }
 
-    public boolean hasToken() {
+    public boolean hasDisc() {
         return token;
     }
 
     public void addToken() {
         this.token = true;
+    }
+
+    public void makeWhite() {
+        this.discColor = 'W';
+    }
+
+    public void makeBlack() {
+        this.discColor = 'B';
+    }
+
+    public char getDiscColor() {
+        return discColor;
     }
 }
