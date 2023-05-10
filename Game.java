62be581979dde33e47b055889d656977d68b2ec1
koakatta <koakatta@gmail.com>
2023-05-10 14:50:02 +0700

    upload img

--- Game.java
+++ Game.java
@@ -1,6 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.Scanner;
 
-public class Game {
+public class Game extends JFrame {
     private Board board;
     private Player player1;
     private Player player2;
@@ -11,6 +13,18 @@ public class Game {
         player1 = new Player('B');
         player2 = new Player('W');
         currentPlayer = player1;
+        GridUI gridUI = new GridUI();
+        add(gridUI);
+
+        pack();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+//        game.play();
     }
 
     public void play() {
@@ -30,8 +44,28 @@ public class Game {
         System.out.println("Game over! The winner is " + board.getWinner());
     }
 
-    public static void main(String[] args) {
-        Game game = new Game();
-        game.play();
-    }
 }
+
+    class GridUI extends JPanel {
+        public GridUI() {
+            setPreferredSize(new Dimension(600, 600));
+
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+            setBackground(new Color(53, 101, 77));
+            g.setColor(Color.black);
+            g.drawRect(50, 50, 50, 50);
+            g.drawRect(100, 50, 50, 50);
+            g.fillOval(50, 50, 50, 50);
+            g.setColor(Color.white);
+
+            g.fillOval(100, 50, 50, 50);
+
+
+        }
+    }
+
+
